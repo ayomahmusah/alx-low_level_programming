f295e5c398be1f3eea86ce59a1702d478221bf06
ayomahmusah <ayomahmubarak7@gmail.com>
2023-03-16 12:55:47 -0700

    first line of c code

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/*
+ * main - first c code with bett style 
+ *
+ * Return:0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual");
+	return (0);
+} 
+
