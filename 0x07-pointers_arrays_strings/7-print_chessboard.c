c9713fdedf7f1bec6fb6133402062a82a58ef447
ayomahmusah <ayomahmubarak7@gmail.com>
2023-04-04 15:26:44 -0700

    7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -4,15 +4,15 @@
  * @a: array
  * Return: Always 0 (Success)
  */
+
 void print_chessboard(char (*a)[8])
 {
-	int m;
-	int n;
-
-	for (m = 0; m < 8; m++)
-	{
-		for (n = 0; n < 8; n++)
-			_putchar(a[m][n]);
-		_putchar('\n');
-	}
+  int i, j;
+  for (i = 0; i < 8; i++) {
+    for (j = 0; j < 8; j++) {
+      printf("%c ", board[i][j]);
+    }
+    printf("\n");
+  }
 }
+
