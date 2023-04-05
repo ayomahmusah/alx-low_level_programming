eea26baf8f301921caf56ea785e13e4a18cd0b12
ayomahmusah <ayomahmubarak7@gmail.com>
2023-04-04 17:36:41 -0700

    8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -6,12 +6,22 @@
  * @size: input
  * Return: Always 0 (Success)
  */
-void print_diagsums(int *a, int size) {
-  int i, total1 = 0, total2 = 0;
-  for (i = 0; i < size * size; i += size + 1) {
-    total1 += a[i];
-    total2 += a[(i / (size - 1) + 1) * (size - 1)];
-  }
-  printf("Sum of diagonal 1: %d\n", total1);
-  printf("Sum of diagonal 2: %d\n", total2);
+void print_diagsums(int *a, int size) 
+{
+	int total1, total2, i;
+
+	total1 = 0;
+	total2 = 0;
+
+	for (y = 0; i < size; i++)
+	{
+		total1 = total1 + a[y * size + y];
+	}
+
+	for (i = size - 1; i >= 0; i--)
+	{
+		total2 += a[i * size + (size - i - 1)];
+	}
+
+	printf("%d, %d\n", total1, total2);
 }
