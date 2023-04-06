210dda5988fb1231a3d1cd9e6d5f4e4948cb4e47
ayomahmusah <ayomahmubarak7@gmail.com>
2023-04-06 08:32:51 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,12 +5,13 @@
  * Return: Always 0 (Success)
  */
 void _puts_recursion(char *s)
-{ 
-	if (*s == '\0')
+{
+	if (*s)
 	{
-		putchar('\n');
-		return;
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
-	putchar(*s);
-	_puts_recursion(s + 1);
+
+	else
+		_putchar('\n');
 }
