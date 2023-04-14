f0c8666806fdc45c63bce1c3a465b52a78846efe
ayomahmusah <ayomahmubarak7@gmail.com>
2023-04-14 16:00:43 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,38 +12,44 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *sout;
-	unsigned int ls1, ls2, lsout, i;
+  char *result;
+  unsigned int s1_len, s2_len, i;
+  if (s1 == NULL)
+  {
+    s1 = "";
+  }
 
-	if (s1 == NULL)
-		s1 = "";
+  if (s2 == NULL)
+  {
+    s2 = "";
+  }
 
-	if (s2 == NULL)
-		s2 = "";
+  s1_len = strlen(s1);
+  s2_len = strlen(s2);
 
-	for (ls1 = 0; s1[ls1] != '\0'; ls1++)
-		;
+  if (n >= s2_len)
+  {
+    n = s2_len;
+  }
 
-	for (ls2 = 0; s2[ls2] != '\0'; ls2++)
-		;
+  result = (char *) malloc(s1_len + n + 1);
 
-	if (n > ls2)
-		n = ls2;
+  if (result == NULL)
+  {
+    return NULL;
+  }
 
-	lsout = ls1 + n;
+  for (i = 0; i < s1_len; i++)
+  {
+    result[i] = s1[i];
+  }
 
-	sout = malloc(lsout + 1);
+  for (i = 0; i < n; i++)
+  {
+    result[s1_len + i] = s2[i];
+  }
 
-	if (sout == NULL)
-		return (NULL);
+  result[s1_len + n] = '\0';
 
-	for (i = 0; i < lsout; i++)
-		if (i < ls1)
-			sout[i] = s1[i];
-		else
-			sout[i] = s2[i - ls1];
-
-	sout[i] = '\0';
-
-	return (sout);
+  return result;
 }
