6a763d7337247a430e2cbfd24250814ad0a3c33a
ayomahmusah <ayomahmubarak7@gmail.com>
2023-04-14 18:22:02 -0700

    fix

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -7,13 +7,13 @@
  * Return: pointer to allocated memory.
  */
 void *malloc_checked(unsigned int b)
-{ 
+{
 	void *p;
 
 	p = malloc(b);
 
-        if (p == NULL)
-  	        exit(98);
+	if (p == NULL)
+		exit(98);
 
 	return (p);
 }
